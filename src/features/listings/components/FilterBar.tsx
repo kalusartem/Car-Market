2bbff3da2e6f2c8ea500661c12f919c6325b4b08
kalusartem <kalusartem@gmail.com>
2026-01-01 14:51:15 -0500

    select cleanup

--- src/features/listings/components/FilterBar.tsx
+++ src/features/listings/components/FilterBar.tsx
@@ -11,15 +11,18 @@ export function FilterBar({ filters, setFilters }: any) {
         />
 
         {/* Make Dropdown */}
-        <select
-          className="bg-slate-800 border-none rounded-lg px-4 py-2 text-white min-w-[150px]"
-          onChange={(e) => setFilters({ ...filters, make: e.target.value })}
-        >
-          <option value="">All Makes</option>
-          <option value="Porsche">Porsche</option>
-          <option value="Rivian">Rivian</option>
-          <option value="Toyota">Toyota</option>
-        </select>
+        <div className="relative inline-block">
+          <select className="appearance-none bg-slate-800 border-none rounded-lg px-4 py-2 pr-10 text-white min-w-[150px]">
+            <option value="">All Makes</option>
+            <option value="Porsche">Porsche</option>
+            <option value="Rivian">Rivian</option>
+            <option value="Toyota">Toyota</option>
+          </select>
+
+          <span className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 text-white">
+            ▼
+          </span>
+        </div>
 
         {/* Price Slider Label */}
         <div className="flex flex-col flex-1 min-w-[200px]">
